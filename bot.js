4fcfe6ae59a15aae893bb494fe7bb37075c38e76
Illia Oleksiuk <42911468+ioleksiuk@users.noreply.github.com>
2019-06-20 15:49:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,34 +2,44 @@ const telegraf = require('telegraf');
 const bot = new telegraf("719324937:AAHozQ9uRxa54Lbgj3YtHW9k4EnydwFDYJs");
 var Feed = require('rss-to-json');
 const fs = require('fs');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
-//how to store API KEY securely
-//how to parse JSON
-//how to work with RSS
+const TOKEN = process.env.TOKEN || 'abcde'
 
-bot.use((ctx, next) => {
+console.log(TOKEN)
+const Job = mongoose.model('Job', { title: String });
+
+
+function parseJobs() {
   const start = new Date()
-  return next(ctx).then(() => {
     Feed.load('https://jobs.dou.ua/vacancies/feeds/?category=Security', function(err, rss){
-      // jobs = JSON.parse(rss)
-      // console.log(rss)
-      // console.log(rss.items)
-      // console.log(rss)
-      // fs.writeFile("/tmp/test", rss.stringify(), function(err) {
       if(err) {
           return console.log(err);
       }
-      ctx.reply(JSON.stringify(rss.items[0].title));
-
-    // console.log("The file was saved!");
-      // var myRe = (?<=\"title\": \")(.*\n?)(?=\") ;
-      // var re = new RegExp('(?<=\"title\"\: \")(.*\n?)(?=\")')
-      // var myArray = myRe.exec(rss);
-          // regexp = "(?<=\"title\"\: \")(.*\n?)(?=\")"
-          // var myArray = regexp.exec(rss)
-          // ctx.reply(rss);
-      });
+      for (element of rss.items) {
+        console.log(element.title)
+        const job = new Job({ name: element.title });
+				Job.find({title: element.title})
+					.then((jobs) => {
+					if (!jobs) {
+						job.save().then(console.log)
+					} else {
+				console.log('OLD JOB')
+				}
+				})
+      }
   })
+}
+Job.find({ })
+	.then(console.log)
+	.catch(console.error)
+
+// parseJobs()
+
+// setInterval(parseJobs, 10 * 1000)
+
+bot.start((ctx, next) => {
 })
 
 
