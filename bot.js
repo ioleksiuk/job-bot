23dd72ad9cb35e2c70c4e6bbd101b69326d9ee16
Illia Oleksiuk <42911468+ioleksiuk@users.noreply.github.com>
2019-06-24 12:39:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,80 @@
-const telegraf = require('telegraf');
-const bot = new telegraf("719324937:AAHozQ9uRxa54Lbgj3YtHW9k4EnydwFDYJs");
+const Telegraf = require('telegraf')
+const Telegram = require('telegraf/telegram')
 var Feed = require('rss-to-json');
-const fs = require('fs');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
 
-const TOKEN = process.env.TOKEN || 'abcde'
+const telegram = new Telegram(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-console.log(TOKEN)
-const Job = mongoose.model('Job', { title: String });
+bot.command('job', (ctx) =>{
 
+  Feed.load('https://jobs.dou.ua/vacancies/feeds/?category=Security', function(err, rss){
+    if(err) {
+        return console.log(err);
+    }
+    for (element of rss.items) {
+      const keyboard = Markup.inlineKeyboard([
+        Markup.urlButton(element.title, element.link)
+        // ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard))
+        // Markup.callbackButton('Delete', 'delete')
+      ])
+      // ctx.reply(Extra.markup(keyboard))
+      // ctx.message.text = " "
+      // ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard), "hello")
+      // console.log(ctx.message)
+       ctx.replyWithHTML('➕' ,Extra.markup(keyboard))
+       console.log(element.description)
 
-function parseJobs() {
-  const start = new Date()
-    Feed.load('https://jobs.dou.ua/vacancies/feeds/?category=Security', function(err, rss){
-      if(err) {
-          return console.log(err);
-      }
-      for (element of rss.items) {
-        console.log(element.title)
-        const job = new Job({ name: element.title });
-				Job.find({title: element.title})
-					.then((jobs) => {
-					if (!jobs) {
-						job.save().then(console.log)
-					} else {
-				console.log('OLD JOB')
-				}
-				})
-      }
-  })
-}
-Job.find({ })
-	.then(console.log)
-	.catch(console.error)
+      // ctx.reply("➕ " + element.title + '\n\n' + element.pubDate + element.link + '\n' )
+    }
+})})
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+// const telegraf = require('telegraf');
+// const bot = new telegraf("719324937:AAHozQ9uRxa54Lbgj3YtHW9k4EnydwFDYJs");
+// var Feed = require('rss-to-json');
+// const fs = require('fs');
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+//
+// const TOKEN = process.env.TOKEN || 'abcde'
+//
+// console.log(TOKEN)
+// const Job = mongoose.model('Job', { title: String });
+//
+//
+// function parseJobs() {
+//   const start = new Date()
+//     Feed.load('https://jobs.dou.ua/vacancies/feeds/?category=Security', function(err, rss){
+//       if(err) {
+//           return console.log(err);
+//       }
+//       for (element of rss.items) {
+//         console.log(element.title)
+//         const job = new Job({ name: element.title });
+// 				Job.find({title: element.title})
+// 					.then((jobs) => {
+// 					if (!jobs) {
+// 						job.save().then(console.log)
+// 					} else {
+// 				console.log('OLD JOB')
+// 				}
+// 				})
+//       }
+//   })
+// }
+// Job.find({ })
+// 	.then(console.log)
+// 	.catch(console.error)
 
 // parseJobs()
 
 // setInterval(parseJobs, 10 * 1000)
 
-bot.start((ctx, next) => {
-})
+// bot.start((ctx, next) => {
+// })
 
 
 
@@ -52,6 +86,3 @@ bot.start((ctx, next) => {
 
 // updateUser(ctx)
 // })
-
-
-bot.launch()
